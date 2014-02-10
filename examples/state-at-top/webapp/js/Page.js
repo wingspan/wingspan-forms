8590dac9e92c6c9e4d89eb2ccf3d11d753373661
Daniel Miladinov <dmiladinov@wingspan.com>
2014-02-10 17:00:42 -0500

    Add an arbitarily nested component

--- examples/state-at-top/webapp/js/Page.js
+++ examples/state-at-top/webapp/js/Page.js
@@ -33,7 +33,10 @@ define([
                     timer2: 2,
                     timer3: 145,
                     timer4: 989,
-                    timer5: 3452
+                    timer5: 3452,
+                    fooForm: {
+                        timer: 0
+                    }
                 };
             },
 
@@ -45,10 +48,29 @@ define([
                         <Timer value={this.state.timer3} onChange={_.partial(this.onChange, 'timer3')} />
                         <Timer value={this.state.timer4} onChange={_.partial(this.onChange, 'timer4')} />
                         <Timer value={this.state.timer5} onChange={_.partial(this.onChange, 'timer5')} />
+                        <FooForm value={this.state.fooForm} onChange={_.partial(this.onChange, 'fooForm')} />
                         <hr />
                         <pre>{JSON.stringify(this.state, undefined, 2)}</pre>
                     </div>
 
+                );
+            }
+        });
+
+
+        var FooForm = React.createClass({
+            getDefaultProps: function () {
+                return {
+                    value: undefined,
+                    onChange: undefined
+                };
+            },
+
+            render: function () {
+                return (
+                    <div className="FooForm">
+                        <Timer value={this.props.value.timer} onChange={_.partial(this.props.onChange, 'timer')}/>
+                    </div>
                 );
             }
         });
