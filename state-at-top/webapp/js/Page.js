92539a39d3be63752731bcab99e5dd2aa7f58937
Dustin Getz <dgetz@wingspan.com>
2014-02-11 15:48:09 -0500

    Formatting

--- state-at-top/webapp/js/Page.js
+++ state-at-top/webapp/js/Page.js
@@ -26,11 +26,14 @@ define([
             },
 
             render: function () {
+                var sum = this.state.foo.timer + this.state.foo.bar.timer + this.state.foo.bar.baz.timer;
                 return (
                     <div className="App">
                         <div>
-                            <Foo value={this.state.foo} onChange={_.partial(this.onChange, 'foo')} />
-                            <p>Sum of timers: {this.state.foo.timer + this.state.foo.bar.timer + this.state.foo.bar.baz.timer}</p>
+                            <Foo
+                                value={this.state.foo}
+                                onChange={_.partial(this.onChange, 'foo')} />
+                            <p>Sum of timers: {sum}</p>
                         </div>
                         <pre>{JSON.stringify(this.state, undefined, 2)}</pre>
                     </div>
@@ -44,8 +47,12 @@ define([
             render: function () {
                 return (
                     <div className="Foo">
-                        <Timer value={this.props.value.timer} onChange={_.partial(this.props.onChange, 'timer')}/>
-                        <Bar value={this.props.value.bar} onChange={_.partial(this.props.onChange, 'bar')} />
+                        <Timer
+                            value={this.props.value.timer}
+                            onChange={_.partial(this.props.onChange, 'timer')}/>
+                        <Bar
+                            value={this.props.value.bar}
+                            onChange={_.partial(this.props.onChange, 'bar')} />
                     </div>
                 );
             }
@@ -55,8 +62,12 @@ define([
             render: function () {
                 return (
                     <div className="Bar">
-                        <Timer value={this.props.value.timer} onChange={_.partial(this.props.onChange, 'timer')}/>
-                        <Baz value={this.props.value.baz} onChange={_.partial(this.props.onChange, 'baz')} />
+                        <Timer
+                            value={this.props.value.timer}
+                            onChange={_.partial(this.props.onChange, 'timer')}/>
+                        <Baz
+                            value={this.props.value.baz}
+                            onChange={_.partial(this.props.onChange, 'baz')} />
                     </div>
                 );
             }
@@ -66,7 +77,9 @@ define([
             render: function () {
                 return (
                     <div className="Baz">
-                        <Timer value={this.props.value.timer} onChange={_.partial(this.props.onChange, 'timer')}/>
+                        <Timer
+                            value={this.props.value.timer}
+                            onChange={_.partial(this.props.onChange, 'timer')}/>
                     </div>
                 );
             }
