543515be4113b79a4ceee9934ecc53ff984202ff
Dustin Getz <dgetz@wingspan.com>
2014-02-11 21:07:41 -0500

    add the Contact mocks

--- faceted-search/webapp/js/Page.js
+++ faceted-search/webapp/js/Page.js
@@ -1,9 +1,52 @@
 /** @jsx React.DOM */
 define([
     'underscore', 'react', 'jquery', 'kendo', 'wingspan-forms',
+    'text!textassets/types/Contact.json',
+    'text!textassets/contacts.json',
     'underscore-string'
-], function (_, React, $, kendo, Forms) {
+], function (_, React, $, kendo, Forms, ContactModel, contacts) {
     'use strict';
+
+    var ContactModel = JSON.parse(ContactModel).data;
+    var contacts = JSON.parse(contacts).data;
+
+
+    function entrypoint(rootElement) {
+
+        var App = React.createClass({
+            mixins: [Forms.TopStateMixin],
+
+            getInitialState: function () {
+                return {
+                    filters: [] // { display: 'Name', value: 'name' }
+                };
+            },
+
+            componentWillMount: function () {
+                this.columns = [{ template: '#: lastName #' }, { template: '#: firstName #' }, { template: '#: phoneNumber #' }, { template: '#: contactGroup #' }, { template: '#: email #' }];
+                //var data = _.filter(this.state.data, function (record) { return record.visible; });
+                this.dataSource = new kendo.data.DataSource({ data: contacts });
+            },
+
+            componentDidUpdate: function () {
+                //var data = _.filter(this.state.data, function (record) { return record.visible; });
+                //this.dataSource.data(this.state.data);
+            },
+
+            render: function () {
+                return (
+                    <div className="App">
+                        <KendoGrid dataSource={this.dataSource} columns={this.columns} />
+                        <div className="facets">
+
+                        </div>
+                    </div>
+                );
+            }
+        });
+
+        React.renderComponent(<App />, rootElement);
+    }
 
 
     var FormField = Forms.FormField;
@@ -21,40 +64,6 @@ define([
     var Carousel = Forms.Carousel;
     var KendoGrid = Forms.KendoGrid;
 
-
-    function entrypoint(rootElement) {
-
-        var App = React.createClass({
-            mixins: [Forms.TopStateMixin],
-
-            getInitialState: function () {
-                return {
-                    data: [{name: 'Danny', value: 'danny'}, {name: 'Mark', value: 'mark'},{name: 'Bob', value: 'bob'},{name: 'JY', value: 'jy'}, {name: 'Jason', value: 'jason'} ]
-                };
-            },
-
-            componentWillMount: function () {
-                this.columns = [{ title: 'name', template: '#: name #' }];
-                //var data = _.filter(this.state.data, function (record) { return record.visible; });
-                this.dataSource = new kendo.data.DataSource({ data: [] });
-            },
-
-            componentDidUpdate: function () {
-                //var data = _.filter(this.state.data, function (record) { return record.visible; });
-                this.dataSource.data([]);
-            },
-
-            render: function () {
-                return (
-                    <div className="App">
-                        <KendoGrid dataSource={this.dataSource} columns={this.columns} />
-                    </div>
-                );
-            }
-        });
-
-        React.renderComponent(<App />, rootElement);
-    }
 
     return {
         entrypoint: entrypoint
