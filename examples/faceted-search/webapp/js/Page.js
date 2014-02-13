7e66df8e5c47f66e7c7774ce0bdb2a5eb3bbefca
Dustin Getz <dgetz@wingspan.com>
2014-02-12 21:45:59 -0500

    move app out of entrypoint

--- examples/faceted-search/webapp/js/Page.js
+++ examples/faceted-search/webapp/js/Page.js
@@ -11,73 +11,73 @@ define([
     var contacts = JSON.parse(contacts).data;
 
 
-    function entrypoint(rootElement) {
+    var App = React.createClass({
+        mixins: [Forms.TopStateMixin],
 
-        var App = React.createClass({
-            mixins: [Forms.TopStateMixin],
+        getInitialState: function () {
+            return {
+                filters: _(ContactModel.properties).chain()
+                    .map(function (fieldInfo, fieldName) { return [fieldName, {}]; })
+                    .object()
+                    .value()
+            };
+        },
 
-            getInitialState: function () {
-                return {
-                    filters: _(ContactModel.properties).chain()
-                        .map(function (fieldInfo, fieldName) { return [fieldName, {}]; })
-                        .object()
-                        .value()
-                };
-            },
+        componentWillMount: function () {
+            this.columns = [{ template: '#: lastName #' }, { template: '#: firstName #' }, { template: '#: phoneNumber #' }, { template: '#: contactGroup #' }, { template: '#: email #' }];
+            this.dataSource = new kendo.data.DataSource({ data: contacts });
+        },
 
-            componentWillMount: function () {
-                this.columns = [{ template: '#: lastName #' }, { template: '#: firstName #' }, { template: '#: phoneNumber #' }, { template: '#: contactGroup #' }, { template: '#: email #' }];
-                this.dataSource = new kendo.data.DataSource({ data: contacts });
-            },
-
-            componentDidUpdate: function () {
-                var filters = _.map(this.state.filters, function (fieldValues, fieldName) {
-                    var activeFieldValues = filterMap(fieldValues, function (pair) { return !!pair[1]; });
-                    return _.map(_.keys(activeFieldValues), function (fieldValue) {
-                        return { field: fieldName, operator: "eq", value: fieldValue };
-                    });
+        componentDidUpdate: function () {
+            var filters = _.map(this.state.filters, function (fieldValues, fieldName) {
+                var activeFieldValues = filterMap(fieldValues, function (pair) { return !!pair[1]; });
+                return _.map(_.keys(activeFieldValues), function (fieldValue) {
+                    return { field: fieldName, operator: "eq", value: fieldValue };
                 });
-                this.dataSource.filter({ logic: 'or', filters: _.flatten(filters) });
-            },
+            });
+            this.dataSource.filter({ logic: 'or', filters: _.flatten(filters) });
+        },
 
-            render: function () {
+        render: function () {
 
-                var facets = _.chain(filterMap(ContactModel.properties, function (pair) { return !pair[1].hidden; }))
-                    .map(function (fieldInfo, fieldName) {
-                        return [fieldName, _.groupBy(contacts, fieldName)]
-                    }).object().value();
+            var facets = _.chain(filterMap(ContactModel.properties, function (pair) { return !pair[1].hidden; }))
+                .map(function (fieldInfo, fieldName) {
+                    return [fieldName, _.groupBy(contacts, fieldName)]
+                }).object().value();
 
-                var facetControls = _.map(facets, function (facetVals, filterField) {
-                    var checkboxes = _.map(facetVals, function (facetVal, facetName) {
-                        return (
-                            <div className="facetFilterControl" key={facetName}>
-                                <CheckBox label={facetName} id={facetName} value={this.state.filters[filterField][facetName]}
-                                        onChange={_.partial(this.onChange, 'filters', filterField, facetName)}/>
-                                <span className="count">{facetVal.length}</span>
-                            </div>
-                        );
-                    }.bind(this));
+            var facetControls = _.map(facets, function (facetVals, filterField) {
+                var checkboxes = _.map(facetVals, function (facetVal, facetName) {
                     return (
-                        <FormField fieldInfo={_.object([['label', ContactModel.properties[filterField].label]])}>
-                            {checkboxes}
-                        </FormField>
+                        <div className="facetFilterControl" key={facetName}>
+                            <CheckBox label={facetName} id={facetName} value={this.state.filters[filterField][facetName]}
+                                onChange={_.partial(this.onChange, 'filters', filterField, facetName)}/>
+                            <span className="count">{facetVal.length}</span>
+                        </div>
                     );
                 }.bind(this));
-
                 return (
-                    <div className="App">
-                        <div>
-                            <div className="facets">
-                                <div>{facetControls}</div>
-                            </div>
-                            <KendoGrid className="KendoGrid" dataSource={this.dataSource} columns={this.columns} height="600"/>
-                        </div>
-                        <pre>{JSON.stringify(this.state, undefined, 2)}</pre>
-                    </div>
+                    <FormField fieldInfo={_.object([['label', ContactModel.properties[filterField].label]])}>
+                        {checkboxes}
+                    </FormField>
                 );
-            }
-        });
+            }.bind(this));
 
+            return (
+                <div className="App">
+                    <div>
+                        <div className="facets">
+                            <div>{facetControls}</div>
+                        </div>
+                        <KendoGrid className="KendoGrid" dataSource={this.dataSource} columns={this.columns} height="600"/>
+                    </div>
+                    <pre>{JSON.stringify(this.state, undefined, 2)}</pre>
+                </div>
+            );
+        }
+    });
+
+
+    function entrypoint(rootElement) {
         React.renderComponent(<App />, rootElement);
     }
 
