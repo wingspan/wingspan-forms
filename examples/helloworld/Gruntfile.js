2f56584677ad257a7bebaf29522899ca06a9638e
Dustin Getz <dgetz@wingspan.com>
2014-02-13 16:43:39 -0500

    helloworld (no requirejs) - update build

--- examples/helloworld/Gruntfile.js
+++ examples/helloworld/Gruntfile.js
@@ -10,13 +10,33 @@ module.exports = function (grunt) {
             '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
             ' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */\n',
 
+        bower: {
+            install: {
+                options: {
+                    copy: false,
+                    install: true,
+                    verbose: false,
+                    cleanTargetDir: false,
+                    cleanBowerDir: false,
+                    bowerOptions: {}
+                }
+            }
+        },
+
+        subgrunt: {
+            options: {},
+            'wingspan-forms': {
+                'bower_components/wingspan-forms': ['default']
+            }
+        },
+
         react: {
             options: {
                 extension: 'js'
             },
             app: {
                 files: {
-                    'js-built': 'js'
+                    'webapp/js-built': 'webapp/js'
                 }
             }
         },
@@ -24,26 +44,64 @@ module.exports = function (grunt) {
         less: {
             development: {
                 options: {
-                    paths: [
-                        'styles'
-                    ],
+                    paths: ['styles'],
                     ieCompat: true,
                     yuicompress: true,
-                    report: 'min'
+                    report: 'min',
+                    relativeUrls: true
                 },
                 files: {
-                    'dist/helloworld.css': 'styles/helloworld.less'
+                    'webapp/styles/App.css': 'webapp/styles/App.less'
                 }
             }
-        }
+        },
+
+        copy: {
+            'libs': {
+                files: [
+                    {
+                        expand: true,
+                        src: [
+                            'bower_components/jquery/jquery.js',
+                            'bower_components/momentjs/moment.js',
+                            'bower_components/underscore/underscore.js',
+                            'bower_components/react/react-with-addons.js',
+                            'bower_components/underscore.string/lib/underscore.string.js',
+                            'bower_components/es5-shim/es5-shim.js'
+                        ],
+                        dest: 'webapp/lib',
+                        flatten: true,
+                        filter: 'isFile'
+                    },
+                    {
+                        expand: true,
+                        cwd: 'bower_components/kendo-ui/src/',
+                        src: ['js/kendo.web.js', 'styles/kendo.common.css', 'styles/kendo.default.css', 'styles/Default/**', 'styles/textures/**'],
+                        dest: 'webapp/lib/kendo-ui/',
+                        flatten: false
+                    },
+                    {
+                        expand: true,
+                        cwd: 'bower_components/wingspan-forms/dist/',
+                        src: ['**'],
+                        dest: 'webapp/lib/wingspan-forms/',
+                        flatten: false
+                    }
+                ]
+            }
+        },
+
+        clean: ['bower_components', 'webapp/js-built', 'webapp/libs', 'webapp/styles/App.css', 'webapp/Page.js']
     });
 
-    // These plugins provide necessary tasks.
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-requirejs');
     grunt.loadNpmTasks('grunt-contrib-less');
     grunt.loadNpmTasks('grunt-react');
+    grunt.loadNpmTasks('grunt-bower-task');
+    grunt.loadNpmTasks('grunt-subgrunt');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-copy');
 
-    // Default task.
-    grunt.registerTask('default', []);
+    grunt.registerTask('default', ['bower:install', 'subgrunt', 'copy', 'react', 'less']);
 };
