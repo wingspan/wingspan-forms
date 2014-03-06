a075b59dcf68c4bab578e177466ce7402e1f6421
Daniel Miladinov <dmiladinov@wingspan.com>
2014-03-06 13:25:58 -0500

    Add the controlled-grid example

---
+++ examples/controlled-grid/js-built/Page.js
@@ -0,0 +1,136 @@
+/** @jsx React.DOM */
+define([
+    'underscore', 'react', 'jquery', 'kendo', 'wingspan-forms',
+    'underscore-string'
+], function (_, React, $, kendo, Forms) {
+    'use strict';
+
+    var FormField = Forms.FormField;
+    var KendoText = Forms.KendoText;
+    var MultilineText = Forms.MultilineText;
+    var MultiSelect = Forms.MultiSelect;
+    var KendoComboBox = Forms.KendoComboBox;
+    var KendoNumber = Forms.KendoNumber;
+    var KendoDate = Forms.KendoDate;
+    var KendoDatetime = Forms.KendoDatetime;
+    var CheckBox = Forms.CheckBox;
+    var Radio = Forms.Radio;
+    var RadioGroup = Forms.RadioGroup;
+    var SwitchBox = Forms.SwitchBox;
+    var Carousel = Forms.Carousel;
+    var KendoGridPicker = Forms.KendoGridPicker;
+
+    function entrypoint(rootElement) {
+
+        var App = React.createClass({displayName: 'App',
+            mixins: [Forms.TopStateMixin],
+
+            getInitialState: function () {
+                return {
+                    dataSource: [
+                        { name: 'Danny', id: 'danny' },
+                        { name: 'Mark',  id: 'mark' },
+                        { name: 'Bob',   id: 'bob' },
+                        { name: 'JY',    id: 'jy' },
+                        { name: 'Jason', id: 'jason' }
+                    ],
+                    selection: '',
+                    visible: []
+                };
+            },
+
+            componentWillMount: function () {
+                this.visibleDataSource = new kendo.data.DataSource({ data: this.visibleValues() });
+                this.pristineDataSource = new kendo.data.DataSource({ data: this.state.dataSource });
+            },
+
+            componentDidUpdate: function () {
+                this.visibleDataSource.data(this.visibleValues());
+
+                if (this.state.selection && !_.contains(_.pluck(this.visibleValues(), 'id'), this.state.selection)) {
+                    this.onChange('selection', '');
+                }
+            },
+
+            visibleValues: function () {
+                var visibleIds = this.visibleIds();
+                return _.filter(this.state.dataSource, function (record) { return _.contains(visibleIds, record.id); });
+            },
+
+            visibleIds: function () {
+                return _.pluck(this.state.visible, 'id');
+            },
+
+            render: function () {
+                var self = this, visibleIds = _.pluck(this.state.visible, 'id');
+
+                var checkboxes = _.map(this.state.dataSource, function (record) {
+                    return (
+                        CheckBox(
+                            {key:record.id,
+                            id:record.id,
+                            label:record.name,
+                            value:_.contains(visibleIds, record.id),
+                            onChange:_.partial(self.onCheckboxChecked, record)} )
+                    );
+                });
+
+                var gridColumns = [
+                    { title: 'Name', template: '#: name #', sortable: false },
+                    { title: 'Id', template: '#: id #', sortable: false }
+                ];
+
+                var selectedPerson = (this.state.selection && _.findWhere(this.state.dataSource, _.object([['id', this.state.selection]]))).name || 'Nobody!';
+
+                return (
+                    React.DOM.div( {className:"App"}, 
+                        FormField( {fieldInfo:_.object([['label', 'Pick a Person']])}, 
+                            KendoComboBox(
+                                {value:this.state.selection,
+                                dataSource:this.visibleDataSource,
+                                onChange:_.partial(this.onChange, 'selection'),
+                                displayField:"name",
+                                valueField:"id"} )
+                        ),
+                        FormField( {fieldInfo:_.object([['label', 'People as Checkboxes']]), isValid:[true, ''], layout:"formField"}, 
+                            checkboxes
+                        ),
+                        FormField( {fieldInfo:_.object([['label', 'People as Grid']])}, 
+                            KendoGridPicker(
+                                {dataSource:this.pristineDataSource,
+                                value:this.state.visible,
+                                columns:gridColumns,
+                                onChange:_.partial(this.onChange, 'visible')} )
+                        ),
+                        FormField( {fieldInfo:_.object([['label', 'Selected Person']])}, 
+                            KendoText( {value:selectedPerson} )
+                        )
+                    )
+                );
+            },
+
+            onCheckboxChecked: function (record, checked) {
+                var newVisible;
+
+                // I want to write this, but reference equality foils me again!
+                // newVisible = (checked ? _.union : _.difference)(this.state.visible, record);
+
+                // So I have to write this instead:
+                if (checked) {
+                    newVisible = _.union(this.state.visible, record);
+                } else {
+                    newVisible = _.compact(_.map(this.state.visible, function (previousRecord) {
+                        return previousRecord.id === record.id ? null : previousRecord;
+                    }));
+                }
+                this.onChange('visible', newVisible);
+            }
+        });
+
+        React.renderComponent(App(null ), rootElement);
+    }
+
+    return {
+        entrypoint: entrypoint
+    };
+});
