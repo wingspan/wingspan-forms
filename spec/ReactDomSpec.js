9407dcd3f621971957fea6c79b2b952498764105
Dustin Getz <dgetz@wingspan.com>
2014-04-06 16:57:37 -0400

    Unit test that can detect exceptions in render

--- spec/ReactDomSpec.js
+++ spec/ReactDomSpec.js
@@ -9,17 +9,16 @@ describe('react', function() {
         ReactTestUtils = React.addons.TestUtils;
     });
 
-    it('can render into fake dom', function() {
-
-        var Throws = React.createClass({
-            render: function () {
-                throw 42;
-                return (<div />);
-            }
-        });
-
-        var label = <div><Throws className="42"/>Some Text We Need for Test</div>;
+    it('can render', function() {
+        var label = <div>Hello</div>;
         ReactTestUtils.renderIntoDocument(label);
+    });
+
+    it('can detect exceptions in render', function () {
+        var Throws = React.createClass({
+            render: function () { throw 42; }
+        });
+        expect(_.partial(ReactTestUtils.renderIntoDocument, <Throws />)).toThrow();
     });
 
     it('can get wingspan-forms on the page', function () {
