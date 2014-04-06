604c0d50b6b6bfd7ea2320494e4dfb0f86da9c92
Dustin Getz <dgetz@wingspan.com>
2014-04-06 18:08:10 -0400

    KendoComboBox - upgrade to react 0.10

--- spec/KendoComboSpec.js
+++ spec/KendoComboSpec.js
@@ -5,6 +5,8 @@ var ReactTestUtils = React.addons.TestUtils;
 
 describe('KendoComboBox', function() {
 
+    var inlineData = [{ id: 10, name: 'Dustin'}, { id: 11, name: 'Jon'}, { id: 12, name: 'Paul'}];
+
     it('must have a valueField and displayField', function () {
         expect(_.partial(ReactTestUtils.renderIntoDocument,
             <KendoComboBox value="42" />
@@ -16,15 +18,36 @@ describe('KendoComboBox', function() {
     });
 
     it('accepts inline data', function () {
-        var dataSource = [{ id: 10, name: 'Dustin'}, { id: 11, name: 'Jon'}, { id: 12, name: 'Paul'}];
         ReactTestUtils.renderIntoDocument(
             <KendoComboBox
                 displayField="name"
                 valueField="id"
-                dataSource={dataSource}
-                value={dataSource[0]} />
+                dataSource={inlineData}
+                value={inlineData[0]} />
         );
     });
+
+    // This should fail. I don't know how to force an update
+    // in test mode.
+//    it('can\'t update inline data', function () {
+//
+//        var div = document.createElement('div');
+//        document.documentElement.appendChild(div);
+//        React.renderComponent(
+//            <KendoComboBox
+//                displayField="name"
+//                valueField="id"
+//                dataSource={inlineData}
+//                value={inlineData[0]} />,
+//            div);
+//        React.renderComponent(
+//            <KendoComboBox
+//                displayField="name"
+//                valueField="id"
+//                dataSource={[{id: 42, name: 'name'}]}
+//                value={inlineData[1]} />,
+//            div);
+//    });
 });
 
 
