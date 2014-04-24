0f145b4e3ca7a5de4e41cd3aea03f0dd8274d563
Daniel Miladinov <dmiladinov@wingspan.com>
2014-04-24 15:34:07 -0400

    Fix Cursor

--- js/Cursor.js
+++ js/Cursor.js
@@ -3,17 +3,17 @@ define([
 ], function (_) {
     'use strict';
 
-    function Cursor (state, path, commit, clone) {
-
+    function Cursor(state, path, commit, clone) {
         this.value = getRefAtPath(state, path);
 
         this.onChange = function (nextValue) {
             var nextState;
+            nextValue = clone(nextValue); // because the call site might retain the reference and mutate
+
             if (path.length > 0) {
-                var nextState = clone(state);
+                nextState = clone(state); // defensive clone before mutate
                 var scoped = getRefAtPath(nextState, _.initial(path));
                 scoped[_.last(path)] = nextValue;
-                commit(nextState);
             }
             else if (path.length === 0) {
                 nextState = nextValue;
@@ -31,13 +31,10 @@ define([
         return new Cursor(state, [], commit, clone);
     };
 
+    function getRefAtPath(tree, paths) {
+        function deref(obj, key) { return obj[key]; }
 
-
-
-    function getRefAtPath (tree, paths) {
         return _.reduce(paths, deref, tree);
-
-        function deref (obj, key) { return obj[key]; }
     }
 
     return Cursor;
