ed23568af486f9ba5abc0a19b31768ca5911dcf7
Daniel Miladinov <dmiladinov@wingspan.com>
2014-05-16 13:12:12 -0400

    Starting point for phase workshop

--- examples/live-code-starter/webapp/js/Page.js
+++ examples/live-code-starter/webapp/js/Page.js
@@ -6,10 +6,34 @@ define([
 
     var FormField = Forms.FormField;
     var KendoText = Forms.KendoText;
+    var KendoComboBox = Forms.KendoComboBox;
+    var AutoControl = Forms.AutoControl;
+
+    var PrettyJSON = React.createClass({
+        render: function () {
+            return <pre>{JSON.stringify(this.props.value, undefined, 2)}</pre>;
+        }
+    });
+
+    var App = React.createClass({
+        getInitialState: function () {
+            return {
+
+            };
+        },
+        render: function () {
+
+        },
+
+        onChange: function (fieldName, fieldValue) {
+
+        }
+    });
+
 
     function entrypoint() {
-        var jsx = (<div>Hello, JSX Transformer!</div>);
-        React.renderComponent(jsx, document.getElementById('root'));
+        React.renderComponent(<App />, document.getElementById('root'));
+        Forms.ControlCommon.attachFormTooltips($('body'));
     }
 
     return {
