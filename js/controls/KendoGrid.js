8505b9dabc1169ac35997d7f32974ad166e4dee0
Andrew Goodale <agoodale@wingspan.com>
2014-07-09 09:07:28 -0400

    KendoGrid refactored to support more kendo grid features including selection. KendoGridPicker re-implemented using KendoGrid. #47 Removed KendoGridPickerByButton and KendoGridRadioSelectable. #21

--- js/controls/KendoGrid.js
+++ js/controls/KendoGrid.js
@@ -1,58 +1,153 @@
 /** @jsx React.DOM */
 define([
-    'underscore', 'jquery', 'react', 'kendo',
-    '../ImmutableOptimizations'
-], function (_, $, React, kendo, ImmutableOptimizations) {
+    'underscore', 'jquery', 'react', 'kendo'
+], function (_, $, React, kendo) {
     'use strict';
+
+    void kendo;
+    var PropTypes = React.PropTypes;
+
+    function eitherType(type1, type2) {
+        type1 = _.isString(type1) ? PropTypes[type1] : type1;
+        type2 = _.isString(type2) ? PropTypes[type2] : type2;
+
+        return PropTypes.oneOfType([type1, type2]);
+    }
+
+    function isCellSelection(selectable) {
+        return _.isString(selectable) ? selectable.indexOf('cell') !== -1 : false;
+    }
+
+    function isMultiSelect(selectable) {
+        return _.isString(selectable) ? selectable.indexOf('multiple') !== -1 : false;
+    }
+
+    function updateGridSelection(component, grid) {
+        // Ignore change events while updating selection
+        grid.unbind('change', component.onGridChange);
+
+        if (_.isEmpty(component.props.value)) {
+            grid.clearSelection();
+            grid.bind('change', component.onGridChange);
+            return;
+        }
+
+        var selectors = _.pluck(component.props.value, 'id')
+            .map(function (id) { return grid.dataSource.get(id); })
+            .filter(function (dataItem) { return !!dataItem; })
+            .map(function (dataItem) { return 'tr[data-uid="' + dataItem.uid + '"]'; });
+
+        grid.select(selectors.join(', '));
+        grid.bind('change', component.onGridChange);
+    }
 
     var KendoGrid = React.createClass({
 
+        propTypes: {
+            className: PropTypes.string,
+            height: eitherType('number', 'string'),
+            dataSource: eitherType(PropTypes.object.isRequired, PropTypes.array.isRequired),
+            autoBind: PropTypes.bool,
+            columns: PropTypes.array,
+            rowTemplate: eitherType('string', 'func'),
+            pageable: eitherType('bool', 'object'),
+            scrollable: eitherType('bool', 'object'),
+            selectable: eitherType('bool', 'string'),
+            sortable: eitherType('bool', 'object'),
+            options: PropTypes.object,
+            value: PropTypes.any,
+            onChange: PropTypes.func
+        },
+
         getDefaultProps: function () {
             return {
-                className: '',
-                height: 150, // TODO remove this
-                dataSource: undefined,
-                columns: undefined,
-                rowTemplate: undefined,
-                selectable: undefined,
-                scrollable: undefined,
-                change: undefined
+                autoBind: true,
+                pageable: false,
+                scrollable: true,
+                selectable: false,
+                sortable: false
             };
         },
 
+        /*jshint ignore:start */
         render: function () {
-            console.assert(this.props.dataSource);
             return (<div className={this.props.className} />);
         },
+        /*jshint ignore:end */
 
         componentDidMount: function () {
             var $rootNode = $(this.getDOMNode());
-            void kendo;
-            $rootNode.kendoGrid({
+            var widgetOptions = _.defaults({
+                autoBind: this.props.autoBind,
                 dataSource: this.props.dataSource,
                 height: this.props.height,
                 columns: this.props.columns,
                 rowTemplate: this.props.rowTemplate,
+                pageable: this.props.pageable,
                 selectable: this.props.selectable,
                 scrollable: this.props.scrollable,
-                change: this.props.change
-            });
+                sortable: this.props.sortable,
+                dataBound: this.onGridDataBound
+            }, this.props.options);
+
+            $rootNode.kendoGrid(widgetOptions);
         },
 
-        componentDidUpdate: function (prevProps, prevState) {
+        componentWillUnmount: function () {
+            $(this.getDOMNode()).data('kendoGrid').destroy();
+        },
+
+        componentDidUpdate: function (prevProps) {
             var $el = $(this.getDOMNode());
-            var kendoWidget = $el.data('kendoGrid');
+            var grid = $el.data('kendoGrid');
 
             if (this.props.dataSource instanceof Array) {
                 if (!_.isEqual(this.props.dataSource, prevProps.dataSource)) {
                     // This better be a datasource that was originally built from inline data.
                     // I don't know how to detect this to verify it.
-                    kendoWidget.dataSource.data(this.props.dataSource);
+                    grid.dataSource.data(this.props.dataSource);
                 }
             }
             else if (prevProps.dataSource !== this.props.dataSource) {
-                kendoWidget.setDataSource(this.props.dataSource);
+                grid.setDataSource(this.props.dataSource);
             }
+
+            if (prevProps.value !== this.props.value) {
+                if (grid.selectable) {
+                    updateGridSelection(this, grid);
+                }
+            }
+        },
+
+        onGridDataBound: function (e) {
+            var grid = e.sender;
+
+            if (grid.selectable) {
+                updateGridSelection(this, grid);
+            }
+        },
+
+        onGridChange: function (e) {
+            var grid = e.sender;
+            var selectedNodes = grid.select().get();    // get() unwraps the jQuery object
+
+            function rowSelection(tr) {
+                return grid.dataItem(tr);
+            }
+            function cellSelection(td) {
+                return {
+                    cellNode: td,
+                    dataItem: grid.dataItem(td.parentNode)
+                };
+            }
+            var selectedValues = selectedNodes.map(isCellSelection(this.props.selectable) ? cellSelection : rowSelection);
+
+            // Don't hand the caller an array if they are doing only single selection. It's nicer that way.
+            if (!isMultiSelect(this.props.selectable) && (selectedValues.length === 1)) {
+                selectedValues = selectedValues[0];
+            }
+
+            this.props.onChange(selectedValues);
         }
     });
 
--- js/controls/KendoGridPicker.js
+++ js/controls/KendoGridPicker.js
@@ -1,148 +1,95 @@
-/** @jsx React.DOM */
 define([
     'underscore',
     'jquery',
     'react',
     'kendo',
-    '../util/util',
-    '../ImmutableOptimizations'
-], function (_, $, React, kendo, util, ImmutableOptimizations) {
+    './KendoGrid'
+], function (_, $, React, kendo, KendoGrid) {
     'use strict';
+
+    var PropTypes = React.PropTypes;
+
+    var KendoGridPickerTemplate = '<div class="checkboxWrap"><input id="#: uid #" type="checkbox" value="#: id #" name="checkboxSelector"><label for="#: uid #"></label></div>';
+
+    function enableCheckboxSelection(grid) {
+        grid.selectable.userEvents.notify = function (eventName, data) {
+            // Make all "tap/click" events seem like they have the ctrlKey pressed, which gives a checkbox-style UX
+            if (eventName === 'tap') {
+                data.event.ctrlKey = true;
+            }
+            this.trigger(eventName, data);
+        };
+    }
 
     var KendoGridPicker = React.createClass({
 
-        $el: null,
+        propTypes: {
+            columns: PropTypes.array.isRequired,
+            value: PropTypes.array,
+            onChange: PropTypes.func
+        },
 
         getDefaultProps: function () {
             return {
                 autoBind: true,
                 editable: false,
                 pageable: false,
-                multiSelect: true, // was false
                 height: 250,
-                onChange: function () {},
+                onChange: $.noop,
                 value: []  // list of selected records, just like combo.
             };
         },
 
-        componentWillMount: function () {
-            console.assert(this.props.dataSource);
-            console.assert(this.props.columns);
-            console.assert(this.props.multiSelect === true); // temporary simplification
-            console.assert(_.isArray(this.props.value));
-        },
-
-        componentWillReceiveProps: function (nextProps) {
-            var cantChange = ['dataSource', 'editable', 'pageable'];
-            console.assert(_.isEqual(_.pick(nextProps, cantChange), _.pick(this.props, cantChange)), 'these props cant change after mount');
-        },
-
-        /*jshint ignore:start */
         render: function () {
-            return (<div className={this.props.className} />);
+            var columns = [{ title: '', template: kendo.template(KendoGridPickerTemplate), width: 34 }].concat(this.props.columns);
+
+            return this.transferPropsTo(KendoGrid({ columns: columns, selectable: 'multiple, row', onChange: this.onGridChange }));
         },
-        /*jshint ignore:end */
 
         componentDidMount: function () {
-            this.$el = $(this.getDOMNode());
+            var grid = $(this.getDOMNode()).data('kendoGrid');
 
-            var columns = [{ title: '', template: kendo.template(KendoGridPickerTemplate), width: 34 }];
-            columns = columns.concat(this.props.columns);
+            // The standard kendo grid multiple selection UI requires holding the Control key to select multiple rows.
+            // Change the behavior to allow individual clicks to toggle the row's selection state.
+            enableCheckboxSelection(grid);
 
-            this.$el.kendoGrid({
-                dataSource: this.props.dataSource,
-                height: this.props.height,
-                columns: columns,
-                sortable: this.props.sortable,
-                editable: this.props.editable,
-                pageable: this.props.pageable,
-                autoBind: this.props.autoBind,
-                dataBound: this.applySelectionStateToDom
-            }).data('kendoGrid');
-
-            if (!this.props.autoBind) {
-                this.$el.data('kendoGrid').refresh();
-            }
-
-            this.$el.on('click', 'tr', this.onRowClick);
-
+            this.updateCheckboxValues();
+            grid.bind('dataBound', this.updateCheckboxValues);
         },
 
-        componentWillUnmount: function () {
-            this.$el.data('kendoGrid').destroy();
-            this.$el = null;
+        componentDidUpdate: function () {
+            this.updateCheckboxValues();
         },
 
-        componentDidUpdate: function (prevProps, prevState) {
-            this.$el = $(this.getDOMNode());
-
-            this.applySelectionStateToDom();
-        },
-
-        applySelectionStateToDom: function () {
+        updateCheckboxValues: function () {
             // the SSP page has changed, so we have new DOM.
             // Sync up the DOM with the checked state.
-            var grid = this.$el.data('kendoGrid');
+            var $rootNode = $(this.getDOMNode());
             var valueIDs = _.pluck(this.props.value, 'id');
 
             // Update the checked state of checkbox inputs
-            this.$el.find('input[type="checkbox"]').val(valueIDs);
-
-            this.$el.find('tr').each(function (i, elem) {
-                var record = grid.dataItem(elem);
-
-                if (record) {
-                    $(elem).toggleClass('k-state-selected', _.contains(valueIDs, record.id));
-                }
-            });
+            $rootNode.find('input[type="checkbox"]').val(valueIDs);
         },
 
-        onRowClick: function (e) {
-            // Prevent "shadow" clicks on the label from changing state;
-            // The real clicks happen on the input itself, another event targeted on the input will be arriving shortly
-            if ('LABEL' === e.target.nodeName || 'A' === e.target.nodeName) {
-                return;
-            }
+        onGridChange: function (selectedValues) {
+            var grid = $(this.getDOMNode()).data('kendoGrid');
 
-            // Get the record associated with this click event
-            var $target = $(e.target);
-            var $row = $target.closest('tr');
+            // The `selectedValues` represents the selection for the current page of results. We need to merge those values
+            // with existing values on separate pages.
+            var modelsOnThisPage = grid.dataSource.view().map(function (model) {
+                return model.id;
+            });
+            var valuesOnOtherPages = this.props.value.filter(function (dataItem) {
+                return modelsOnThisPage.indexOf(dataItem.id) === -1;
+            });
 
-            // Ignore clicks on the row containing header cells
-            // clicking on the header should not cause a change in selections
-            if (!_.some($row.find('.k-header'))) {
-                var model = this.$el.data('kendoGrid').dataItem($row);
-                var record = _.extend(model.toJSON(), { id: model.id });
+            selectedValues = selectedValues.map(function (model) {
+                return model.toJSON();
+            }).concat(valuesOnOtherPages);
 
-                // Determine the current selection state of this record
-                var wasSelected = util.containsDeep(this.props.value, record);
-
-                //if this event was on the checkbox, revert that dom state change until it goes through the flux loop
-                if ('INPUT' === e.target.nodeName) {
-                    e.target.checked = wasSelected;
-                }
-
-                // Toggle the state
-                var isSelected = !wasSelected;
-
-                // Invoke our event handler with the new selection state for our control.  This will circle back
-                // and re-render us with the new selections
-                var nextSelections = (isSelected ? util.unionDeep : util.differenceDeep)(this.props.value, [record]);
-                this.props.onChange(nextSelections);
-            }
-
+            this.props.onChange(selectedValues);
         }
     });
 
-
-    var KendoGridPickerTemplate = '\
-        <div class="checkboxWrap">\
-            <input id="#: uid #" type="checkbox" value="#: id #" name="checkboxSelector">\
-                <label for="#: uid #"></label>\
-            </div>';
-
-
-
     return KendoGridPicker;
-
 });
