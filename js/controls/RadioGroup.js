217e419e9759a960b777a54276f159f1d9046e98
Andrew Goodale <agoodale@wingspan.com>
2015-01-20 16:10:53 -0500

    RadioGroup can generate radio buttons from an array datasource. Fixes #81.

--- js/controls/RadioGroup.js
+++ js/controls/RadioGroup.js
@@ -1,35 +1,47 @@
-/** @jsx React.DOM */
 define([
-    'underscore', 'react'
-], function (_, React) {
+    'react',
+    './Radio'
+], function (React, Radio) {
     'use strict';
 
+    var PropTypes = React.PropTypes;
 
-    var RadioGroup = React.createClass({
+    var RadioGroup = React.createClass({displayName: "RadioGroup",
 
         propTypes: {
-            value: React.PropTypes.any
+            name: PropTypes.string,
+            value: PropTypes.any,
+            onChange: PropTypes.func,
+            dataSource: PropTypes.arrayOf(PropTypes.shape({
+                value: PropTypes.any,
+                label: PropTypes.string
+            })),
+            className: PropTypes.string
         },
 
         statics: { fieldClass: function () { return 'formFieldRadio'; } },
 
-        /*jshint ignore:start */
         render: function () {
-            var value = this.props.value;
+            var props = this.props;
+
+            function renderRadio(option) {
+                return (React.createElement(Radio, {
+                    key: option.value,
+                    name: props.name,
+                    value: option.value,
+                    onChange: props.onChange,
+                    checked: (option.value == props.value)
+                }, option.label));
+            }
 
             React.Children.forEach(this.props.children, function (radio) {
-
                 // The use of double-equals is intentional here, so that numbers represented as strings will match.
-                radio.props.checked = (radio.props.value == value);
+                radio.props.checked = (radio.props.value == props.value);
             });
 
-            return (
-                <fieldset>
-                    {this.props.children}
-                </fieldset>
-            );
+            return React.createElement("fieldset", { className: props.className },
+                this.props.dataSource ? this.props.dataSource.map(renderRadio) : this.props.children);
         }
-        /*jshint ignore:end */
     });
 
 
